6885fe0a08ebda39ae4d5415e03f4d46035f2c15
vineet_jain <vineetjain170@gmail.com>
2017-06-12 12:25:38 +0530

    java code, no improved UI, no jason parsers added

--- app/src/main/java/com/vscholars/stack2code/aicte_phaseone/MainActivity.java
+++ app/src/main/java/com/vscholars/stack2code/aicte_phaseone/MainActivity.java
@@ -3,11 +3,21 @@ package com.vscholars.stack2code.aicte_phaseone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends DoubleNavigationWithoutEditText {
+
+    //This is the main activity of AICTE Dashboard to display statistics
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This message is passed between activities to indicate which element of dashboard is under consideration
+        super.onCreateDrawer(MainActivity.this,"dashboard");
+
+        getSupportActionBar().setTitle("Aicte Dashboard");
+    }
+    public void onBackPressed(){
+        moveTaskToBack(true);
     }
 }
